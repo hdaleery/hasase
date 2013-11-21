df6c26666792de58313d52d2e898e3fa041da937
hameddaleeryan <hameddaleeryan@10.16.138.245>
2013-11-21 15:02:19 -0500

    second try

---
+++ Lab4/Lab4_main.cpp
@@ -0,0 +1,77 @@
+/*
+ * Lab4_main.cpp
+ *
+ *  Created on: Nov 21, 2013
+ *      Author: hameddaleeryan
+ */
+
+
+/*
+ * bloodtest.cpp
+ *
+ *  Created on: Nov 12, 2013
+ *      Author: hameddaleeryan
+ */
+
+#include "bloodtest.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <iosfwd>
+#include "functions.hpp"
+using namesapace std;
+
+
+		int main() {
+	int choose;
+	int i;
+}
+		// file opened here
+myfile.open;
+while(1) {
+	// call base function here
+	bloodTest callfunc;
+	callfunc.get(;)
+	completebloodcount callCBC;
+	whitebloodcellcount callWBC;
+	redbloodcellcount callRBC;
+
+	// main menu
+	cout<< "select the blood type"<<endl;
+	cout<< "A complete blood count"<<endl;
+	cout<< "B white blood cell test"<<endl;
+	cout<< "C red blood cell test"<<endl;
+	cout<< "D exit" <<endl;
+	cin >> choose;
+	myfile.open ("bloodtest.txt");
+	switch(choose){
+	case 1:
+		callCBC.getCBC();
+		callCBC.test.CBC();
+		// callfunc.opentofile
+		break;
+
+	case 2:
+		callWBC.getWBC();
+	    callWBC.test();
+	  break;
+	case 3;
+	callRBC.getRBC();
+	callRBC.testRBC();
+	break;
+	case 5:
+		i=0;
+		break;
+	}
+}
+myfile.close();
+return0;
+}
+
+
+
+
+
+
+
