57f7c5cc0ea86e076aa04be3794804337c777dce
hameddaleeryan <hameddaleeryan@10.16.138.245>
2013-11-21 15:03:25 -0500

    thirs try

---
+++ Lab4/bloodtest.cpp
@@ -0,0 +1,57 @@
+/*
+ * bloodtest.cpp
+ *
+ *  Created on: Nov 21, 2013
+ *      Author: hameddaleeryan
+ */
+
+
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <iosfwd>
+#include "bloodtest.hpp"
+using namespace std;
+//constructor
+
+bloodTest:: bloodTest(){
+	madmin = "unknown";
+	mTIme = 0;
+	mDate = 0;
+
+}
+completebloodcount:: completebloodcount(){
+	mgender = "unknown";
+	mwhiteblood = 0;
+	mredblood = 0;
+	mplatelet = 0;
+}
+whitebloodcell:: whitevloodcell(){
+	mcalcium = 0;
+	mchloride = 0;
+	mmagnesium = 0;
+}
+redbloodcell:: redbloodcell(){
+	mtotalcholesterol = 0;
+	mLDLcholesterol = 0;
+	mHDLcholesterol = 0;
+}
+
+}
+void bloodtest:: get(){
+	cout<< "please select one of the following"<< endl;
+	cout<< "A physician"<< endl;
+	cout<< "B Radiologist"<< endl;
+	cout<< "nurse"<<endl;
+	cin>> "mAdmin";
+	cout<< "please enter the date"<<endl;
+	cin>> mdate;
+	cout<< "please enter the time"<<endl;
+	cin>> mtime;
+}
+void completebloodcout:: getCBC(){
+	cout<<
+}
